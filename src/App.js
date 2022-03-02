b941b870ebde6983652c659c5421771d8922f1b8
Wilhelm A. Ngoma <lawellwilhelm.usual@gmail.com>
2022-03-02 09:03:44 -0800

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Let's build a linkedin clone</h1>
+    </div>
+  );
+};
+export default App;
