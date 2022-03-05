b085d7fc573c0f30c1707b21452db749e79361d6
Wilhelm A. Ngoma <lawellwilhelm.usual@gmail.com>
2022-03-04 17:38:30 -0800

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const App = () => {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
 
       {/* App Body */}
+      <div className="app-body">
+        <Sidebar />
+      </div>
       {/* Sidebar */}
       {/* Feed */}
       {/* Wdigets */}
